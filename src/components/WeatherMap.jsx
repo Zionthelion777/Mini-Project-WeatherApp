f3efc3f312c4a75aa3297d1cf48016b9bbe6f42e
Jenny Lin <linjenny332@gmail.com>
2024-06-17 14:31:41 -0500

    Updated to conform to react

--- src/components/WeatherMap.jsx
+++ src/components/WeatherMap.jsx
@@ -1,5 +1,8 @@
 import { Link } from 'react-router-dom'
 
+
+
+
 function WeatherMap() {
     return (
         <div>
@@ -9,3 +12,24 @@ function WeatherMap() {
 }
 
 export default WeatherMap
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Weather Map</title>
+    <link rel="stylesheet" href="../index.css">
+</head>
+<body>
+    <div id="app">
+        <h1>Weather Map</h1>
+        <input type="text" id="city" placeholder="Enter city name">
+        <button id="getWeather">Get Weather</button>
+        <div id="weatherResult"></div>
+    </div>
+    <script src="../script.js"></script>
+</body>
+</html>
+
