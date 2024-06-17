4bff7e8e1153cb15cfd2cc83dc7eb42d5e6ee92c
Annika Cruz <anncruz@publicisgroupe.net>
2024-06-17 14:18:06 -0500

    Set up React app

---
+++ src/components/WeatherMap.jsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom'
+
+function WeatherMap() {
+    return (
+        <div>
+            <h1>Weather Map Page</h1>
+        </div>
+    )
+}
+
+export default WeatherMap
