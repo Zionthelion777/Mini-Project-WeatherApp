cceef875a9085d63f2abcb77216ed0a0f9c0e1af
Jenny Lin <linjenny332@gmail.com>
2024-06-20 10:40:25 -0500

    Working on the displaying the map on the browser

---
+++ my-app/main.js
@@ -0,0 +1,29 @@
+import Map from 'ol/Map.js';
+import OSM from 'ol/source/OSM.js';
+import TileLayer from 'ol/layer/Tile.js';
+import View from 'ol/View.js';
+
+const map = new Map({
+  layers: [
+    new TileLayer({
+      source: new OSM(),
+    }),
+  ],
+  target: 'map',
+  view: new View({
+    center: [0, 0],
+    zoom: 2,
+  }),
+});
+
+document.getElementById('zoom-out').onclick = function () {
+  const view = map.getView();
+  const zoom = view.getZoom();
+  view.setZoom(zoom - 1);
+};
+
+document.getElementById('zoom-in').onclick = function () {
+  const view = map.getView();
+  const zoom = view.getZoom();
+  view.setZoom(zoom + 1);
+};
